453967b2e0dccae111be4d77c414ee66f8ec4e52
Gusttavo Bona <Gusttavo11bona@gmail.com>
2025-04-01 21:11:16 -0300

    Update index.py

--- PROJETOFINAL/index.py
+++ PROJETOFINAL/index.py
@@ -1,11 +1,15 @@
-import sqlite3
-import tkinter as tk
-from tkinter import messagebox, simpledialog
+import sqlite3 # banco de dados
+import tkinter as tk # interface basica
+from tkinter import messagebox # caixas de mensagens
+from tkinter import ttk # interface grafica tb
 
-def create_db():
-    conn = sqlite3.connect('meu_banco.db')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS pessoas (
+def conectar():
+    return sqlite3.connect('teste.db')
+
+def criar_tabela():
+    conn = conectar()
+    c= conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS pessoas (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         nome TEXT NOT NULL,
                         email TEXT,
@@ -13,162 +17,143 @@ def create_db():
                         endereco TEXT)''')
     conn.commit()
     conn.close()
+  
 
-def salvar():
+
+# CREATE
+def inserir_usuario():
     nome = entry_nome.get()
     email = entry_email.get()
     telefone = entry_telefone.get()
     endereco = entry_endereco.get()
 
-    if nome:  
-        conn = sqlite3.connect('meu_banco.db')
-        cursor = conn.cursor()
-
-        cursor.execute('''INSERT INTO pessoas (nome, email, telefone, endereco) 
-                          VALUES (?, ?, ?, ?)''', (nome, email, telefone, endereco))
-
+    if nome and email:  
+        conn = conectar()
+        c = conn.cursor()
+       
+        c.execute('INSERT INTO pessoas (nome, email, telefone, endereco) VALUES (?, ?, ?, ?)', 
+                  (nome, email, endereco,telefone))
         conn.commit()
         conn.close()
-
-        messagebox.showinfo('CONFIRMADO', 'Cadastro realizado com sucesso')
-        listar_nomes()
-        limpar_campos()
+        messagebox.showinfo('AVISO', 'DADOS INSERIDOS COM SUCESSO!') 
+        mostrar_usuario()  
     else:
-        messagebox.showerror('Erro', 'O nome não pode ser vazio')
+        messagebox.showerror('ERRO', 'NOME E EMAIL SÃO OBRIGATÓRIOS!') 
 
-def listar_nomes():
-    conn = sqlite3.connect('meu_banco.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM pessoas")
-    registros = cursor.fetchall()
-    conn.close()
 
-    listbox.delete(0, tk.END)
-    for registro in registros:
-        listbox.insert(tk.END, f"ID: {registro[0]}, Nome: {registro[1]}")
-
-def deletar_item():
-    conn = sqlite3.connect('meu_banco.db')
-    cursor = conn.cursor()
-
-    selecionado = listbox.curselection()
-
-    if selecionado:
-        item = listbox.get(selecionado)
-        item_id = item.split(",")[0].split(":")[1].strip()
-
-        cursor.execute("DELETE FROM pessoas WHERE id=?", (item_id,))
-        conn.commit()
-        listbox.delete(selecionado)
-
-    conn.close()
-
-def editar_item():
-    selected_index = listbox.curselection()
-
-    if selected_index:
-        current_value = listbox.get(selected_index)
-        item_id = current_value.split(",")[0].split(":")[1].strip()
-
-        conn = sqlite3.connect('meu_banco.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT nome, email, telefone, endereco FROM pessoas WHERE id=?", (item_id,))
-        cliente = cursor.fetchone()  
-        conn.close()
-
-        if cliente:
-            nome_atual, email_atual, telefone_atual, endereco_atual = cliente
-            
-            nome = simpledialog.askstring("Editar Item", "Digite o novo nome:", initialvalue=nome_atual)
-            email = simpledialog.askstring("Editar Item", "Digite o novo email:", initialvalue=email_atual)
-            telefone = simpledialog.askstring("Editar Item", "Digite o novo telefone:", initialvalue=telefone_atual)
-            endereco = simpledialog.askstring("Editar Item", "Digite o novo endereço:", initialvalue=endereco_atual)
-
-            if nome and email and telefone and endereco:
-                conn = sqlite3.connect('meu_banco.db')
-                cursor = conn.cursor()
-                cursor.execute("UPDATE pessoas SET nome=?, email=?, telefone=?, endereco=? WHERE id=?",
-                               (nome, email, telefone, endereco, item_id))
-                conn.commit()
-                conn.close()
-
-                listbox.delete(selected_index)
-                listbox.insert(selected_index, f"ID: {item_id}, Nome: {nome}")
-
-                messagebox.showinfo("Sucesso", "Item atualizado com sucesso!")
-            else:
-                messagebox.showerror("Erro", "Todos os campos devem ser preenchidos!")
-        else:
-            messagebox.showerror("Erro", "Cliente não encontrado no banco de dados.")
-    else:
-        messagebox.showerror("Erro", "Nenhum item selecionado!")
-
-def visualizar_cliente():
-    try:
-        selecionado = listbox.curselection()
+# READ
+def mostrar_usuario():
+    for row in tree.get_children():   
+        tree.delete(row)
+    conn = conectar()
+    c = conn.cursor()    
+    c.execute('SELECT * FROM pessoas')  
+    usuarios = c.fetchall()
+    
+    for usuario in usuarios:
+       
+        tree.insert("", "end", values=(usuario[0], usuario[1], usuario[2], usuario[3], usuario[4]))
         
-        if not selecionado:
-            raise ValueError("Nenhum item selecionado.")
+    conn.close()
 
-        item_texto = listbox.get(selecionado) 
-        item_id = item_texto.split(",")[0].split(":")[1].strip() 
 
-        conn = sqlite3.connect('meu_banco.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT nome, email, telefone, endereco FROM pessoas WHERE id=?", (item_id,))
-        cliente = cursor.fetchone()  
-        conn.close()
 
-        if cliente:
-            nome, email, telefone, endereco = cliente
-            messagebox.showinfo("Informações do Cliente", f"Nome: {nome}\nEmail: {email}\nTelefone: {telefone}\nEndereço: {endereco}")
+# DELETE
+def delete_usuario():
+    dado_del = tree.selection()
+    if dado_del:
+       user_id = tree.item(dado_del)['values'][0]
+       conn = conectar()
+       c = conn.cursor()    
+       c.execute('DELETE FROM pessoas WHERE id = ? ',(user_id,))
+       conn.commit()
+       conn.close()
+       messagebox.showinfo('', 'DADO DELETADO')
+       mostrar_usuario()
+
+    else:
+       messagebox.showerror('', 'OCORREU UM ERRO')  
+
+# UPDATE  
+def editar():
+    selecao = tree.selection() 
+    if selecao:
+        user_id = tree.item(selecao)['values'][0]
+        novo_nome = entry_nome.get()
+        novo_email = entry_email.get()
+        novo_endereco = entry_endereco.get()
+        novo_telefone = entry_telefone.get()
+
+        if novo_nome and novo_email and novo_endereco and novo_telefone:
+            conn = conectar()
+            c = conn.cursor()
+            # Atualizando corretamente os 4 campos
+            c.execute('UPDATE pessoas SET nome = ?, email = ?, telefone = ?, endereco = ? WHERE id = ?',
+                      (novo_nome, novo_email, novo_telefone, novo_endereco, user_id))
+            conn.commit()
+            conn.close()
+            messagebox.showinfo('', 'DADOS ATUALIZADOS')
+            mostrar_usuario()  # Atualiza a visualização na árvore
         else:
-            messagebox.showerror("Erro", "Cliente não encontrado no banco de dados.")
+            messagebox.showwarning('', 'PREENCHA TODOS OS CAMPOS')  
+    else:
+        messagebox.showerror('', 'SELECIONE UM USUÁRIO NA LISTA!')  
 
-    except Exception as e:
-        messagebox.showerror("Erro", str(e))
+# VAMOS COMPLETAR A INTERFACE GRÁFICA...
+# 1
+janela = tk.Tk()
+janela.title('CRUD')
 
-def limpar_campos():
-    entry_nome.delete(0, tk.END)
-    entry_email.delete(0, tk.END)
-    entry_telefone.delete(0, tk.END)
-    entry_endereco.delete(0, tk.END)
 
-root = tk.Tk()
-root.title('Cadastro de pessoas')
 
-tk.Label(root, text='Nome: ').grid(row=0, column=0, padx=10, pady=10)
-entry_nome = tk.Entry(root)
+
+
+label_nome = tk.Label(janela, text='Nome:')
+label_nome.grid(row=0, column=0, padx=10, pady=10)
+entry_nome = tk.Entry(janela)
 entry_nome.grid(row=0, column=1, padx=10, pady=10)
 
-tk.Label(root, text='Email: ').grid(row=1, column=0, padx=10, pady=10)
-entry_email = tk.Entry(root)
+label_email = tk.Label(janela, text='E-mail:')
+label_email.grid(row=1, column=0, padx=10, pady=10)
+entry_email = tk.Entry(janela)
 entry_email.grid(row=1, column=1, padx=10, pady=10)
 
-tk.Label(root, text='Telefone: ').grid(row=2, column=0, padx=10, pady=10)
-entry_telefone = tk.Entry(root)
-entry_telefone.grid(row=2, column=1, padx=10, pady=10)
 
-tk.Label(root, text='Endereço: ').grid(row=3, column=0, padx=10, pady=10)
-entry_endereco = tk.Entry(root)
-entry_endereco.grid(row=3, column=1, padx=10, pady=10)
+label_endereco = tk.Label(janela, text='Endereco:')
+label_endereco.grid(row=2, column=0, padx=10, pady=10)
 
-btn_salvar = tk.Button(root, text='Salvar', command=salvar)
-btn_salvar.grid(row=4, column=0, columnspan=2, pady=10)
+entry_endereco= tk.Entry(janela, text='Endereco:')
+entry_endereco.grid(row=2, column=1, padx=10, pady=10)
 
-btn_deletar = tk.Button(root, text='Excluir usuario', command=deletar_item)
-btn_deletar.grid(row=5, column=0, columnspan=2, pady=10)
+label_telefone = tk.Label(janela, text='Telefone:')
+label_telefone.grid(row=3, column=0, padx=10, pady=10)
+entry_telefone = tk.Entry(janela)
+entry_telefone.grid(row=3, column=1, padx=10, pady=10)
 
-btn_editar = tk.Button(root, text='Editar', command=editar_item)
-btn_editar.grid(row=6, column=0, columnspan=2, pady=10)
 
-btn_visualizar = tk.Button(root, text='Visualizar', command=visualizar_cliente)
-btn_visualizar.grid(row=8, column=0, columnspan=2, pady=10)
 
-listbox = tk.Listbox(root, width=40, height=10)
-listbox.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+# botões
 
-create_db()
-listar_nomes()
+btn_salvar = tk.Button(janela, text='SALVAR',command=inserir_usuario)
+btn_salvar.grid(row = 4, column=0, padx=10, pady=10  )
 
-root.mainloop()
+btn_deletar = tk.Button(janela, text='DELETAR',command=delete_usuario)
+btn_deletar.grid(row = 4, column=1, padx=10, pady=10  )
+
+btn_atualizar = tk.Button(janela, text='ATUALIZAR',command=editar)
+btn_atualizar.grid(row = 4, column=2, padx=10, pady=10  )
+
+# arvore
+columns = ('ID','NOME', 'EMAIL', 'TELEFONE', 'ENDERECO')
+
+tree = ttk.Treeview(janela, columns=columns, show='headings')
+tree.grid(row=5, column=0,columnspan=2, padx=10, pady=10 )
+
+for col in columns:
+    tree.heading(col, text=col)
+
+criar_tabela()
+mostrar_usuario()
+
+
+janela.mainloop()
